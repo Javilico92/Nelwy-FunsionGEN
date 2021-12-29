b1af5f1e9282f424c47cbed6df6f268014e1cae8
javilico92 <javilico92@gmail.com>
2021-12-30 00:18:54 +0100

    Added NelwyCore as emulator.

--- application/config/emulator_names.php
+++ application/config/emulator_names.php
@@ -1,5 +1,6 @@
 <?php
 $emulators = array(
+	"nelwy_rbac_soap" => "NelwyCore (3.3.5a and 4.3.4) with RBAC - connected through SOAP",
 	"trinity_rbac_soap" => "TrinityCore (3.3.5a) with RBAC - connected through SOAP",
 	"azerothcore_soap" => "AzerothCore (3.3.5a) - connected through SOAP"
 );
